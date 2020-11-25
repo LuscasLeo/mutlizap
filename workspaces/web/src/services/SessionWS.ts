c01ab3f8c93184a24f09fec176292995267404cf
LucasSilva <lucas76leonardo@gmail.com>
2020-11-25 16:44:03 -0300

    Updated tsconfig rootDir

--- workspaces/web/src/services/SessionWS.ts
+++ workspaces/web/src/services/SessionWS.ts
@@ -4,66 +4,66 @@ import { ClientEventsTypes, ServerEventTypes } from "@common/communication";
 import { ICloseEvent } from "websocket";
 
 export type SessionWSEventTypes = {
-    "close": (event: ICloseEvent) => void,
-    "disconnect": (event: ICloseEvent) => void
+	"close": (event: ICloseEvent) => void,
+	"disconnect": (event: ICloseEvent) => void
 }
 
 export class SessionWS extends EventEmitter {
-    private client: WebSocketClient<any>;
-    private connected: boolean;
-
-    constructor (connectionUrl: string) {
-    	super();
-    	this.client = new WebSocketClient(jsonParser, connectionUrl);
-    	this.connected = false;
-    }
-
-    async connect () {
-    	return new Promise<void>((resolve, reject) => {
-    		this.client.on("connect", () => {
-    			this.connected = true;
-    			resolve();
-    		});
-
-    		this.client.on("error", reject);
-
-    		this.client.on("close", (event) => {
-    			this.emit("close", event);
-    			if (this.connected) {
-    				this.connected = false;
-    				this.emit("disconnect", event);
-    			}
-    		});
-
-    		this.client.on("message", ({ header, payload }) => {
-    			this.emit(`MESSAGE_${header}`, payload);
-    		});
-
-    		this.client.connect();
-    	});
-    }
-
-    on<U extends keyof SessionWSEventTypes> (event: U, listener: SessionWSEventTypes[U]) {
-    	super.on(event, listener);
-    	return this;
-    }
-
-    onMessage<U extends keyof ClientEventsTypes> (header: U, listener: (payload: ClientEventsTypes[U]) => void) {
-    	super.on(`MESSAGE_${header}`, listener);
-    	return this;
-    }
-
-    onceMessage<U extends keyof ClientEventsTypes> (header: U, listener: (payload: ClientEventsTypes[U]) => void) {
-    	super.once(`MESSAGE_${header}`, listener);
-    	return this;
-    }
-
-    sendMessage<U extends keyof ServerEventTypes> (header: U, payload?: ServerEventTypes[U]) {
-    	this.client.send({
-    		header,
-    		payload
-    	});
-    }
+	private client: WebSocketClient<any>;
+	private connected: boolean;
+	
+	constructor (connectionUrl: string) {
+		super();
+		this.client = new WebSocketClient(jsonParser, connectionUrl);
+		this.connected = false;
+	}
+	
+	async connect () {
+		return new Promise<void>((resolve, reject) => {
+			this.client.on("connect", () => {
+				this.connected = true;
+				resolve();
+			});
+			
+			this.client.on("error", reject);
+			
+			this.client.on("close", (event) => {
+				this.emit("close", event);
+				if (this.connected) {
+					this.connected = false;
+					this.emit("disconnect", event);
+				}
+			});
+			
+			this.client.on("message", ({ header, payload }) => {
+				this.emit(`MESSAGE_${header}`, payload);
+			});
+			
+			this.client.connect();
+		});
+	}
+	
+	on<U extends keyof SessionWSEventTypes> (event: U, listener: SessionWSEventTypes[U]) {
+		super.on(event, listener);
+		return this;
+	}
+	
+	onMessage<U extends keyof ClientEventsTypes> (header: U, listener: (payload: ClientEventsTypes[U]) => void) {
+		super.on(`MESSAGE_${header}`, listener);
+		return this;
+	}
+	
+	onceMessage<U extends keyof ClientEventsTypes> (header: U, listener: (payload: ClientEventsTypes[U]) => void) {
+		super.once(`MESSAGE_${header}`, listener);
+		return this;
+	}
+	
+	sendMessage<U extends keyof ServerEventTypes> (header: U, payload?: ServerEventTypes[U]) {
+		this.client.send({
+			header,
+			payload
+		});
+	}
 }
 
 export default SessionWS;
@@ -74,6 +74,6 @@ export const jsonParser: MessageParser<any> = {
 };
 
 export interface JsonMessage {
-    header: string
-    payload: any
+	header: string
+	payload: any
 }
--- workspaces/web/src/services/ws.ts
+++ workspaces/web/src/services/ws.ts
@@ -4,59 +4,59 @@ import { EventEmitter } from "events";
 import { ICloseEvent, IMessageEvent, w3cwebsocket as W3cwebsocket } from "websocket";
 
 export interface MessageParser<T> {
-    encode: (data: T) => any
-    decode: (messageEvent: IMessageEvent) => T
+	encode: (data: T) => any
+	decode: (messageEvent: IMessageEvent) => T
 }
 
 export type SessionEvents<T> = {
-    "close": (event: ICloseEvent) => void
-    "message": (data: T) => void
-    "connect": () => void
-    "error": (error: Error) => void
+	"close": (event: ICloseEvent) => void
+	"message": (data: T) => void
+	"connect": () => void
+	"error": (error: Error) => void
 }
 
 export class WebSocketClient<T> extends EventEmitter {
-    private connection: W3cwebsocket;
-    private parser: MessageParser<T>;
-    private connectionUrl: string;
-
-    constructor (parser: MessageParser<T>, connectionUrl: string) {
-   		super();
-   		this.parser = parser;
-   		this.connectionUrl = connectionUrl;
-    }
-
-    connect () {
-   		this.connection = new W3cwebsocket(this.connectionUrl);
-   		this.connection.onopen = () => {
-   			this.emit("connect");
-   		};
-
-   		this.connection.onerror = (error) => {
-   			this.emit("error", error);
-   		};
-
-   		this.connection.onmessage = (message) => {
-   			this.emit("message", this.parser.decode(message));
-   		};
-
-   		this.connection.onclose = (event) => {
-   			this.emit("close", event);
-   		};
-    }
-
-    on<U extends keyof SessionEvents<T>> (event: U, listener: SessionEvents<T>[U]) {
-   		super.on(event, listener);
-   		return this;
-    }
-
-    send (data: T) {
-   		this.connection.send(this.parser.encode(data));
-    }
-
-    close () {
-   		this.connection.close();
-    }
+	private connection: W3cwebsocket;
+	private parser: MessageParser<T>;
+	private connectionUrl: string;
+	
+	constructor (parser: MessageParser<T>, connectionUrl: string) {
+		super();
+		this.parser = parser;
+		this.connectionUrl = connectionUrl;
+	}
+	
+	connect () {
+		this.connection = new W3cwebsocket(this.connectionUrl);
+		this.connection.onopen = () => {
+			this.emit("connect");
+		};
+		
+		this.connection.onerror = (error) => {
+			this.emit("error", error);
+		};
+		
+		this.connection.onmessage = (message) => {
+			this.emit("message", this.parser.decode(message));
+		};
+		
+		this.connection.onclose = (event) => {
+			this.emit("close", event);
+		};
+	}
+	
+	on<U extends keyof SessionEvents<T>> (event: U, listener: SessionEvents<T>[U]) {
+		super.on(event, listener);
+		return this;
+	}
+	
+	send (data: T) {
+		this.connection.send(this.parser.encode(data));
+	}
+	
+	close () {
+		this.connection.close();
+	}
 }
 
 export default WebSocketClient;
